412a332a9c2f8f766b8696ebb93f482a9b5b7a57
senabahadiroglu <senabahadiroglu0@gmail.com>
2021-01-25 01:29:49 +0300

    16 Ocat Ders Sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snippet - hazır kodlar demek (prop -tab tab)
+    class Product  //Entity
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }   // birim fiyatı
+
+        public int UnitsInStock { get; set; }  //Ürün Stok Adedi
+
+        //CRUID Operasyonları -creat -read -update -delete 
+
+    }
+}
---
+++ OOP2/TuzelMusteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+
+    //Coorperate - Tüzel Müşteri - Legal entity?
+    //miras - inheritance
+    class TuzelMusteri:Musteri
+    {
+        
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+    }
+}
