47cee581271d8d4e116e95a9f7ada9e483703bdd
senabahadiroglu <senabahadiroglu0@gmail.com>
2021-01-25 04:04:13 +0300

    Ders3Odev3

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Id + " Id'sine sahip müşreti eklendi.");
+        }
+
+        public void List(Customer customer)
+        {
+            Console.WriteLine(customer.Id + " Id'li müsterinin" +customer.Name + " ismi listeye eklendi.");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Id + " Id'li müsterinin" + customer.TcNo + " nuamralı Tc Kimlik Numarası silindi.");
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Sena";
+            customer1.Surname = "Bahadıroğlu";
+            customer1.TcNo = "45678912356";
+            customer1.PhoneNumber = "65356566";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Engin";
+            customer2.Surname = "Demiroğ";
+            customer2.TcNo = "65464654654";
+            customer2.PhoneNumber = "5454456";
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.List(customer2);
+            customerManager.Delete(customer2);
+        }
+    }
+}
